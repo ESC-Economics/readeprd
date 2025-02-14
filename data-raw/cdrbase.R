904b1caa989e138786a107b82297a1483d37addc
Miguel Ayala (ESC) <miguel.ayala@esc.vic.gov.au>
2025-02-14 14:58:34 +1100

    update cdr base data

--- data-raw/cdrbase.R
+++ data-raw/cdrbase.R
@@ -5,4 +5,5 @@ library(readr)
 # read base uris
 base_uris <- readr::read_csv("data-raw/cdr_base.csv") %>% janitor::clean_names()
 
-save(base_uris, file = "data/baseuris.rda")
+
+usethis::use_data(base_uris, internal = FALSE)

fec18429cdac3cc5e49916fac01a536173a3b7c4
Miguel Ayala (ESC) <miguel.ayala@esc.vic.gov.au>
2025-02-17 16:10:40 +1100

    move functions to new scripts

---
+++ R/read_api_plandetail.R
@@ -0,0 +1,34 @@
+#' Call EPRD API plan detail
+#' @param base_uri character. A retailer cdr brand name. See `readeprd::base_uris` for valid names.
+#' @param planid character; Supply an EME or VEFS offer identifier
+#'
+
+# need  retailer base_uri and plan_id
+
+call_eprd_api_plandetail <- function(base_uri, planid){
+
+  # Create the base request
+  req <- httr2::request(paste0(get_base_url(), base_uri))
+
+
+  # Choose the appropriate method and perform the request
+  resp_plandetail <- req |>
+
+    httr2::req_url_path_append("cds-au/v1/energy/plans") |>
+
+    httr2::req_url_path_append(planid) |>
+
+    httr2::req_headers(
+      `x-v` = 3,
+      `x-min-v` = 3
+    ) %>%
+    httr2::req_perform()
+
+  # Check for errors based on the status code
+  httr2::resp_check_status(resp_plandetail)
+
+  # Return the parsed content of the response
+  httr2::resp_body_json(resp_plandetail)
+
+}
+
--- R/read_plandetail.R
+++ R/read_plandetail.R
@@ -1,39 +1,24 @@
-#' Call EPRD API
-#' @param base_uri character. A retailer cdr brand name. See `readeprd::base_uris` for valid names.
-#' @param planid character; Supply an EME or VEFS offer identifier
+#
+#' nest contract details into the plan id
 #'
 
-# need  retailer base_uri and plan_id
+read_eprd_plan <- function(base_uri,
+                           planid
+){
 
-call_eprd_api_plandetail <- function(base_uri, planid){
+  plan <- plan_to_df(base_uri, planid) %>%
+    data.table::as.data.table() %>%
+    .[, ':=' (contract = list(tidy_details_to_df(contract)))
+    ] %>%
+    dplyr::as_tibble()
+  #dplyr::mutate(contract = list(tidy_details_to_df(contract)))
 
-  # Create the base request
-  req <- httr2::request(paste0(get_base_url(), base_uri))
-
-
-  # Choose the appropriate method and perform the request
-  resp_plandetail <- req |>
-
-    httr2::req_url_path_append("cds-au/v1/energy/plans") |>
-
-    httr2::req_url_path_append(planid) |>
-
-    httr2::req_headers(
-      `x-v` = 3,
-      `x-min-v` = 3
-    ) %>%
-    httr2::req_perform()
-
-  # Check for errors based on the status code
-  httr2::resp_check_status(resp_plandetail)
-
-  # Return the parsed content of the response
-  httr2::resp_body_json(resp_plandetail)
+  return(plan)
 
 }
 
 
-# Clean Plan detail -----------------------------------------------------------
+
 
 # function that clean retailer plans and adds plan details into a list
 
@@ -115,19 +100,4 @@ tidy_details_to_df <- function(result) {
   }
 
 
-# nest contract details into the plan id
-read_eprd_plan <- function(base_uri,
-                           planid
-                           ){
-
-  plan <- plan_to_df(base_uri, planid) %>%
-    data.table::as.data.table() %>%
-    .[, ':=' (contract = list(tidy_details_to_df(contract)))
-      ] %>%
-    dplyr::as_tibble()
-    #dplyr::mutate(contract = list(tidy_details_to_df(contract)))
-
-  return(plan)
-
-}
 

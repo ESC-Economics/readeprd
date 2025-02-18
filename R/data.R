0853d29e6d7db388746ceadc07606687e7062c83
Miguel Ayala (ESC) <miguel.ayala@esc.vic.gov.au>
2025-02-18 22:09:25 +1100

    documenting base_uris dataset

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' base_uris
+#'
+#' CDR Base URI data from https://www.aer.gov.au/energy-product-reference-data
+#'
+#' @format A data frame:
+#' \describe{
+#' \item{retailer_name}{Name of the energy retailer}
+#' \item{retailer_base_uri}{URI for API}}
+#' \item{cdr_brand}{URI name for API}}
+"base_uris"
